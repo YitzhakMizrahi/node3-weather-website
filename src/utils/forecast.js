5399483fa7b4d9882c8d61250d629bbbe1f1a1bb
Yitzhak Mizrahi <yitzhak@firmusai.com>
2020-09-16 16:44:24 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=1e1ec60412f4e0ef19b0f1a0f3e4963b&query=${latitude},${longitude}`
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.error) {
+            callback(body.error.info, undefined)
+        } else {
+            const { weather_descriptions ,temperature, feelslike } = body.current
+            callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out.`)
+        }
+    })
+}
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoieW1penJhaGkiLCJhIjoiY2tlc3BxbWc0MWVsbzJ5b2VwbWZjOW9uOCJ9.mmWXOq26SOClb4Gq5oAeOw&limit=1`
+
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                longitude: body.features[0].center[0],
+                latitude: body.features[0].center[1],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
